4f304d6f3258a49386c35460d7faca0d939c60b5
dexterschmundt <dexter.schmundt@gmail.com>
2026-06-23 04:42:49 +0200

    es funktionierrt

--- scripts/PhotonMapperDirectWithoutAccumulate.py
+++ scripts/PhotonMapperDirectWithoutAccumulate.py
@@ -4,11 +4,11 @@ from falcor import *
 def render_graph_DefaultRenderGraph():
     g = RenderGraph('DefaultRenderGraph')
     g.create_pass('VBufferRT', 'VBufferRT', {'outputSize': 'Default', 'samplePattern': 'Center', 'sampleCount': 16, 'useAlphaTest': True, 'adjustShadingNormals': True, 'forceCullMode': False, 'cull': 'Back', 'useTraceRayInline': False, 'useDOF': True})
+    g.create_pass('AccumulatePass', 'AccumulatePass', {'enabled': True, 'outputSize': 'Default', 'autoReset': True, 'precisionMode': 'Single', 'maxFrameCount': 0, 'overflowMode': 'Stop'})
     g.create_pass('PhotonMapperDirect', 'PhotonMapperDirect', {})
-    g.create_pass('ToneMapper', 'ToneMapper', {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Aces', 'clamp': True, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
-    g.add_edge('PhotonMapperDirect.color', 'ToneMapper.src')
     g.add_edge('VBufferRT.vbuffer', 'PhotonMapperDirect.vbuffer')
-    g.mark_output('ToneMapper.dst')
+    g.add_edge('PhotonMapperDirect.color', 'AccumulatePass.input')
+    g.mark_output('AccumulatePass.output')
     g.mark_output('PhotonMapperDirect.color')
     return g
 
