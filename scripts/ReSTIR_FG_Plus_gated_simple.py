9aa90220d81ff6a291133a3d04532b246c07c889
dexterschmundt <dexter.schmundt@gmail.com>
2026-08-30 13:42:52 +0200

    .

--- scripts/ReSTIR_FG_Plus_gated_simple.py
+++ scripts/ReSTIR_FG_Plus_gated_simple.py
@@ -1,18 +1,18 @@
 from pathlib import WindowsPath, PosixPath
 from falcor import *
 
-def render_graph_ReSTIR_FG_Plus():
-    g = RenderGraph('ReSTIR_FG_Plus')
-    g.create_pass('ReSTIR_FG_Plus', 'ReSTIR_FG_Plus', {})
+def render_graph_DefaultRenderGraph():
+    g = RenderGraph('DefaultRenderGraph')
     g.create_pass('VBufferRT', 'VBufferRT', {'outputSize': 'Default', 'samplePattern': 'Center', 'sampleCount': 16, 'useAlphaTest': True, 'adjustShadingNormals': True, 'forceCullMode': False, 'cull': 'Back', 'useTraceRayInline': False, 'useDOF': True})
     g.create_pass('ToneMapper', 'ToneMapper', {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Aces', 'clamp': True, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
-    g.add_edge('VBufferRT.vbuffer', 'ReSTIR_FG_Plus.vbuffer')
-    g.add_edge('VBufferRT.mvec', 'ReSTIR_FG_Plus.mvec')
-    g.add_edge('VBufferRT.viewW', 'ReSTIR_FG_Plus.view')
-    g.add_edge('ReSTIR_FG_Plus.color', 'ToneMapper.src')
+    g.create_pass('ReSTIR_FG_Plus_gated_simple', 'ReSTIR_FG_Plus_gated_simple', {})
+    g.add_edge('VBufferRT.vbuffer', 'ReSTIR_FG_Plus_gated_simple.vbuffer')
+    g.add_edge('VBufferRT.mvec', 'ReSTIR_FG_Plus_gated_simple.mvec')
+    g.add_edge('VBufferRT.viewW', 'ReSTIR_FG_Plus_gated_simple.view')
+    g.add_edge('ReSTIR_FG_Plus_gated_simple.color', 'ToneMapper.src')
     g.mark_output('ToneMapper.dst')
     return g
 
-ReSTIR_FG_Plus = render_graph_ReSTIR_FG_Plus()
-try: m.addGraph(ReSTIR_FG_Plus)
+DefaultRenderGraph = render_graph_DefaultRenderGraph()
+try: m.addGraph(DefaultRenderGraph)
 except NameError: None
